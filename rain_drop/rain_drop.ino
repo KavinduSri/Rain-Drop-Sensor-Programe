ab65090b799371431db047bbb399a6188ace12da
Kavindu Sri <88576941+KavinduSri@users.noreply.github.com>
2021-10-05 16:41:14 +0530

    Add files via upload

---
+++ rain_drop/rain_drop.ino
@@ -0,0 +1,25 @@
+#define led 4
+
+const int buz=7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buz,OUTPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  int sv= analogRead(A0);
+  Serial.println(sv);
+  if (sv<900){
+    Serial.println("Raining...!");
+    tone(buz,800);
+    digitalWrite(led,HIGH);
+  }else{
+    digitalWrite(led,LOW);
+    noTone(buz);
+    
+  }
+
+
+}
